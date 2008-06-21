e51d02761490c1d39d3c6b61fb657d064fc6c437
Rob Young <rob@roryoung.co.uk>
2008-06-21 13:41:34 +0100

    got composite feed parser working

--- bin/test.php
+++ bin/test.php
@@ -2,9 +2,10 @@
 require 'lib/FeedParser.class.php';
 
 $parser = new FeedParser();
-$parser->add('http://rss.slashdot.org/Slashdot/slashdot');
+$parser->add('http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/front_page/rss.xml');
+$parser->add('http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/business/rss.xml');
 echo "loaded\n";
 
 foreach ($parser as $item) {
-	echo "foo\n";
+    echo "[" . $parser->current_feed->title() . "] " . $item->title() . "\n";
 }
--- lib/FeedParser.class.php
+++ lib/FeedParser.class.php
@@ -6,18 +6,25 @@ class FeedParser implements Iterator
 	private $urls   = array();
 	private $urls_i = 0;
 	private $current_feed;
-	private $current_item;
 
 	public function add($url)
 	{
 		$this->urls[] = $url;
 		$this->next();
-	}
-	
+  }
+
+  public function __get($name)
+  {
+    switch ($name) {
+    case 'current_feed':
+      return $this->current_feed;
+    }
+  }
+
 	/* Iterator Methods */
 	public function current()
 	{
-		return $this->current_item;
+		return $this->current_feed->current();
 	}
 	
 	public function key()
@@ -26,29 +33,35 @@ class FeedParser implements Iterator
 	}
 	
 	public function next()
-	{
-		$this->current_item = null;
-		if (!$this->current_feed) {
-			if (!isset($this->urls[$this->urls_i])) return;
-			$this->current_feed = Zend_Feed::import($this->urls[$this->urls_i++]);
-		}
-		$this->current_feed->next();
-		if (!$this->current_feed->valid()) {
-			return $this->next();
-		} else {
-			$this->current_item = $this->current_feed->current();
-		}
+  {
+    if ($this->current_feed && $this->current_feed->valid()) {
+      $this->current_feed->next();
+    } else {
+      if (!isset($this->urls[$this->urls_i])) {
+        $this->current_feed = null;
+      } else {
+        $this->current_feed = Zend_Feed::import($this->urls[$this->urls_i++]);
+        $this->current_feed->rewind();
+      }
+    }
 	}
 
 	public function rewind()
 	{
 		$this->current_feed = null;
-		$this->current_item = null;
 		$this->urls_i       = 0;
 	}
 	
 	public function valid()
-	{
-		return (bool)$this->current_item;
+  {
+    if (!$this->current_feed || !$this->current_feed->valid()) {
+      $this->next();
+      if (!$this->current_feed) {
+          return false;
+      }
+      $return =  $this->valid();
+      return $return;
+    }
+    return true;
 	}
 }
