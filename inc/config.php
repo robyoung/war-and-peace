26a27a4be9b58ad3b904d21f956b1c73b1bed216
Arron Woods <arron@MBPARRON.local>
2008-06-22 00:59:11 +0100

    config

--- inc/config.php
+++ inc/config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:inc/config.php
 <?php
 
 $config['domain'] = 'http://localhost/mashed/war-and-peace/';
@@ -7,6 +6,3 @@ $config['path'] = '/Users/arron/www/mashed/war-and-peace/';
 //database server$config['server'] = "localhost";$config['user'] = "root";$config['pass'] = "root";$config['database'] = "mashed";$config['tablePrefix'] = "";
 
 ?>
-=======
-<?php$config['domain'] = 'http://localhost:8080/war-and-peace/';$config['path'] = '/www/mashed/war-and-peace/';//database server$config['server'] = "localhost";$config['user'] = "root";$config['pass'] = "root";$config['database'] = "mashed";$config['tablePrefix'] = "";?>
->>>>>>> 74928631017930e11a9d2d8775b3d07994757ca4:inc/config.php
