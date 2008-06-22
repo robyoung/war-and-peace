c8f16ca35282c8542a08474a9a23bbc966110a88
Rob Young <rob@roryoung.co.uk>
2008-06-22 01:05:48 +0100

    removed config.php

--- inc/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$config['path'] = '/Users/arron/www/mashed/war-and-peace/';
-
-//database server$config['server'] = "localhost";$config['user'] = "root";$config['pass'] = "root";$config['database'] = "mashed";$config['tablePrefix'] = "";
-
-?>
